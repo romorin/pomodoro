c43d12a50b5b92c5f03754604c7a22272bafa1fe
romorin <romorin@zoho.com>
2016-08-14 22:18:39 -0400

    minimalist css + seperate files for html template + css

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    templateUrl: 'app/app.component.html',
+		styleUrls:  ['app/app.component.css']
 })
-export class AppComponent { }
+
+export class AppComponent {
+	statusLabel: string;
+	countdown: number;
+	leftDecoration: string;
+	rightDecoration: string;
+	stateLabel: string;
+	editLabel: string;
+
+	constructor() {
+		this.statusLabel="go work";
+		this.countdown= 1000;
+		this.leftDecoration= "<";
+		this.rightDecoration= ">";
+		this.stateLabel= "state";
+		this.editLabel= "edit";
+	}
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { CountdownMinutesPipe } from './countdown-minutes.pipe';
+import { CountdownSecondsPipe } from './countdown-seconds.pipe';
 
 @NgModule({
   imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, CountdownMinutesPipe, CountdownSecondsPipe ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
