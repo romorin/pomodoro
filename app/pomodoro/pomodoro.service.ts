e00da75dcbb5fdb13ee1efef72ff382c8d1e6002
romorin <romorin@zoho.com>
2016-08-29 18:42:54 -0400

    injecting display

--- app/pomodoro/pomodoro.service.ts
+++ app/pomodoro/pomodoro.service.ts
@@ -12,22 +12,21 @@ import { PomodoroDisplay } from './pomodoro-display';
 @Injectable()
 export class PomodoroService {
 	private _pomodoro: Pomodoro;
-	private _display: PomodoroDisplay;
 
 	constructor(
-			private counterFactory: CounterFactory,
-			private runningPomodoroFactory: RunningPomodoroFactory,
-			private editPomodoroFactory: EditingPomodoroFactory,
-			private pomodoroFactory: PomodoroFactory,
-			private constants: Constants,
-			private workCounterConstants: WorkCounterConstants,
-			private pauseCounterConstants: PauseCounterConstants
+			counterFactory: CounterFactory,
+			runningPomodoroFactory: RunningPomodoroFactory,
+			editPomodoroFactory: EditingPomodoroFactory,
+			pomodoroFactory: PomodoroFactory,
+			constants: Constants,
+			workCounterConstants: WorkCounterConstants,
+			pauseCounterConstants: PauseCounterConstants,
+			private _display: PomodoroDisplay
 		) {
 
 		let workCounter = counterFactory.init(workCounterConstants);
 		let pauseCounter = counterFactory.init(pauseCounterConstants);
 
-		this._display = new PomodoroDisplay();
 		let runPomodoro = runningPomodoroFactory.init(workCounter, pauseCounter, this._display, constants);
 		let editPomodoro = editPomodoroFactory.init(workCounter, pauseCounter, this._display, constants);
 
