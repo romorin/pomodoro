564c86175ca14e4d10ae5b93866b4868197ef740
romorin <romorin@zoho.com>
2016-09-07 12:41:13 -0400

    karma testing works (with only test test)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,9 +4,12 @@
 import { addProviders, async, inject } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
+import { PomodoroService } from './pomodoro/pomodoro.service';
+import { MODULE_POMODORO } from './pomodoro/pomodoro.module';
+
 describe('App: Pomodoro2', () => {
 	beforeEach(() => {
-		addProviders([AppComponent]);
+		addProviders([AppComponent, PomodoroService, MODULE_POMODORO]);
 	});
 
 	it('should create the app',
